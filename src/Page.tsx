f853d3302ee0f1715c0449beb52249df487c8b4c
lovespaces <lvspa01@gmail.com>
2026-03-03 15:38:57 +0900

    help me from this maze

--- src/Page.tsx
+++ src/Page.tsx
@@ -6,7 +6,7 @@ import Socials from "./components/socials";
 function Page() {
     return (
         <>
-            <div className="text-white font-ubuntu min-h-dvh w-full bg-linear-to-t from-gray-800 from-5% via-gray-900 via-10% to-black to-80% grid grid-cols-1 md:grid-cols-4 p-4 pb-0">
+            <div className="overscroll-none text-white font-ubuntu w-full grid grid-cols-1 md:grid-cols-4 p-4 pb-0">
                 <Name />
                 <AboutMe />
                 <Socials />
